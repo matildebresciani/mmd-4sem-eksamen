0f48fe3f804d4fa4376a1b961979d51f1a1c5788
Alberte <albo0002@stud.kea.dk>
2025-12-02 13:39:01 +0100

    feat: button styling

--- apps/web/src/components/atoms/frontend/buttons/BaseButton.tsx
+++ apps/web/src/components/atoms/frontend/buttons/BaseButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { cn } from '@/lib/utilities/ui';
 import Link from 'next/link';
-import Arrow from '../icons/Arrow';
+
 
 type Props = {
     type?: 'button' | 'submit' | 'link';
@@ -9,21 +9,25 @@ type Props = {
     href?: string;
     openNewTab?: boolean | null;
     className?: string;
-    addArrow?: boolean;
+    variant?: 'primary' | 'secondary';
     onClick?: () => void;
 };
 
-const BaseButton = ({ type, title, href, openNewTab, className, addArrow, onClick }: Props) => {
+const BaseButton = ({ type, title, href, openNewTab, className, onClick, variant = 'primary' }: Props) => {
     const style = cn(
-        'flex items-center justify-center gap-6 w-max max-w-full py-4 px-7 font-medium text-lg leading-[178%] border border-white bg-oak-cta-bg rounded-2xl cursor-pointer md:px-23',
-        className,
+         'inline-flex cursor-pointer w-auto font-bold',
+         variant === 'primary' && 'bg-button-primary text-button-text hover:bg-button-primary-hover px-8 py-4 md:px-16 md:py-6 justify-center',
+            variant === 'secondary' &&
+            'bg-button-secondary text-button-text hover:bg-button-secondary-hover px-8 py-4 md:px-16 md:py-6  justify-center',
+         className,
+      
     );
 
     if (type === 'link' && href) {
         return (
             <Link href={href} className={style} target={openNewTab ? '_blank' : '_self'} onClick={() => onClick?.()}>
                 {title}
-                {addArrow && <Arrow className="h-5 w-5 min-w-5" />}
+             
             </Link>
         );
     }
@@ -31,7 +35,7 @@ const BaseButton = ({ type, title, href, openNewTab, className, addArrow, onClic
     return (
         <button type={type && type !== 'link' ? type : 'button'} className={style} onClick={() => onClick?.()}>
             {title}
-            {addArrow && <Arrow className="h-5 w-5 min-w-5" />}
+           
         </button>
     );
 };
