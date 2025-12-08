b0fb3b3252ffe763cc8ccbafda47ecef89ef7c07
matilde <mabr0005@stud.ek.dk>
2025-12-08 20:41:35 +0100

    feat: spacing between article sections

--- apps/web/src/components/organisms/blocks/article-hero/ArticleHero.tsx
+++ apps/web/src/components/organisms/blocks/article-hero/ArticleHero.tsx
@@ -20,7 +20,7 @@ const ArticleHeroBlock: BC<ArticleHeroProps> = async ({ block, locale, pageId })
     console.log('Article Data in Hero Block:', articleData);
 
     return (
-        <BaseBlock>
+        <BaseBlock classNameOuter="!pb-section-xxs">
             <div className="oakgrid gap-0">
                 <div
                     className={cn(
