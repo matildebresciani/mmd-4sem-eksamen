3445eef9c8bc97dded7d3e640ce7b61d3a78cb63
matilde <mabr0005@stud.ek.dk>
2025-12-09 14:05:17 +0100

    fix: spacing

--- apps/web/src/components/organisms/blocks/volunteers-team/VolunteersTeam.tsx
+++ apps/web/src/components/organisms/blocks/volunteers-team/VolunteersTeam.tsx
@@ -12,16 +12,16 @@ const VolunteersTeamBlock: BC<VolunteersTeamProps> = ({ block, locale }) => {
         <BaseBlock>
             <div className="oakgrid">
                 <div className="col-span-12">
-                    <div className="p-l">
-                        {volunteersTeam?.map((volunteer) => {
-                            if (typeof volunteer === 'string') return null;
-                            return <VolunteerCard key={volunteer.id} volunteer={volunteer} variant="small" />;
-                        })}
-                    </div>
+                    {volunteersTeam?.map((volunteer) => {
+                        if (typeof volunteer === 'string') return null;
+                        return <VolunteerCard key={volunteer.id} volunteer={volunteer} variant="small" />;
+                    })}
+                </div>
+                <div className="col-span-12 md:col-start-4 md:col-span-6 mt-l">
                     {addLink && link && (
                         <div className="flex flex-col text-center">
                             {footerText && <h3 className="uppercase">{footerText}</h3>}
-                            <DynamicButton className="w-fit self-center m-section-xxs" link={link} />
+                            <DynamicButton className="w-fit self-center mt-m" link={link} />
                         </div>
                     )}
                 </div>
