b36738d949738d033cd9abe44d9ad51d7a66735b
Katinka <kabj0002@stud.kea.dk>
2025-12-12 14:12:57 +0100

    fix:GalleryMobile + primary button featuredCard

--- apps/web/src/components/molecules/frontend/article-cards/FeaturedCard.tsx
+++ apps/web/src/components/molecules/frontend/article-cards/FeaturedCard.tsx
@@ -36,7 +36,7 @@ const FeaturedCard = ({ article, showLabel, className }: Props) => {
                 <div className="flex flex-col flex-grow">
                     <h3 className="uppercase pb-m">{article.title}</h3>
                     <p className="pb-base">{article.contentMeta?.excerpt}</p>
-                    <BaseButton title="Læs artikel" className="mt-auto w-fit" />
+                    <BaseButton variant="primary" title="Læs artikel" className="mt-auto w-fit" />
                 </div>
             </div>
             <div className="col-span-12 md:col-span-7 md:order-2 order-1 relative overflow-hidden w-full border-b md:border-b-0 md:border-l min-h-[400px] md:min-h-[500px]">
